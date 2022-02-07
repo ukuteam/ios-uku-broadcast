d7fc4d6625068067a8e2b6f05af859bc50914e2f
pOxaes <hugo.pievic@gmail.com>
2022-02-07 10:26:53 +0100

    Initial Commit

---
+++ uku-broadcast/uku_broadcast.h
@@ -0,0 +1,18 @@
+//
+//  uku_broadcast.h
+//  uku-broadcast
+//
+//  Created by Hugo Pievic on 07/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for uku_broadcast.
+FOUNDATION_EXPORT double uku_broadcastVersionNumber;
+
+//! Project version string for uku_broadcast.
+FOUNDATION_EXPORT const unsigned char uku_broadcastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <uku_broadcast/PublicHeader.h>
+
+
